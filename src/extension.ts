b634d5558055ead0e46b184204eedcd167904204
f-velka <82233876+f-velka@users.noreply.github.com>
2022-07-10 21:28:10 +0900

    clean code

--- src/extension.ts
+++ src/extension.ts
@@ -1,76 +1,14 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { File } from './file';
-import { calcValueWidth } from './utils';
 import { parse } from './parser';
-import { getPrinter, PrettyPrinter } from './printer';
+import { getPrinter } from './printer';
 import { FormatType, HeaderLocation, Options } from './options';
-import { getTextWriter } from './textWriter';
-
-// constants
-const DIFF_SCHEME = 'csv-pretty-diff';
-const CSV_DIFF_EXT = '.pretty';
-
-class PrettyDiffProvider implements vscode.TextDocumentContentProvider {
-	// Uri.path => text
-	_texts = new Map<string, string>();
-
-	// emitter and its event
-	onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-	onDidChange = this.onDidChangeEmitter.event;
-
-	provideTextDocumentContent(uri: vscode.Uri): string {
-		return this._texts.get(uri.path) ?? '';
-	}
-
-	registerFiles(fileA: File, fileB: File, options: Options, printer: PrettyPrinter): [vscode.Uri, vscode.Uri] {
-		const mergedWidths = this.mergeMaxWidths(fileA, fileB);
-		const uriA = this.registerFile(fileA, mergedWidths, options, printer);
-		const uriB = this.registerFile(fileB, mergedWidths, options, printer);
-		return [uriA, uriB];
-	}
-
-	private calcMaxValueWidths(file: File): number[] {
-		const maxWidths = new Array<number>(file.columnCount).fill(0);
-		for (const record of file.records) {
-			for (const [index, value] of record.entries()) {
-				const valueWidth = calcValueWidth(value);
-				if (valueWidth > maxWidths[index]) {
-					maxWidths[index] = valueWidth;
-				}
-			}
-		}
-
-		return maxWidths;
-	}
-
-	private mergeMaxWidths(fileA: File, fileB: File): number[] {
-		const a = this.calcMaxValueWidths(fileA);
-		const b = this.calcMaxValueWidths(fileB);
-		const [longer, shorter] = a.length > b.length ? [a, b] : [b, a];
-		return longer.map((w, i) => {
-			if (i >= shorter.length) {
-				return w;
-			}
-			return w > shorter[i] ? w : shorter[i];
-		});
-	}
-
-	private registerFile(file: File, maxWidths: number[], options: Options, printer: PrettyPrinter): vscode.Uri {
-		const writer = getTextWriter();
-		printer.print(file, maxWidths, options, writer);
-
-		const uri = vscode.Uri.parse(`${DIFF_SCHEME}: ${file.fileName}${CSV_DIFF_EXT}`);
-		this._texts.set(uri.path, writer.toString());
-
-		return uri;
-	}
-};
+import { PrettyDiffProvider } from './prettyDiffProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	// register the custom provider
 	const diffProvider = new PrettyDiffProvider();
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(DIFF_SCHEME, diffProvider));
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(PrettyDiffProvider.diffScheme, diffProvider));
 
 	// register diff commands
 	context.subscriptions.push(
@@ -186,7 +124,7 @@ async function executeDiffCommands(provider: PrettyDiffProvider, delimiter: stri
 	const fileB = await parse(fileNameB, textB, delimiter, options);
 	const [uriA, uriB] = provider.registerFiles(fileA, fileB, options, getPrinter(options));
 
-	const title = `${path.basename(fileA.fileName)}${CSV_DIFF_EXT} ⇔ ${path.basename(fileB.fileName)}${CSV_DIFF_EXT}`;
+	const title = `${path.basename(fileA.fileName)}${PrettyDiffProvider.diffExtension} ⇔ ${path.basename(fileB.fileName)}${PrettyDiffProvider.diffExtension}`;
 	vscode.commands.executeCommand('vscode.diff', uriA, uriB, title);
 }
 
