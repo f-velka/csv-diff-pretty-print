bb0e446db6f1c8dcaa9cf66160eec44e3baeb45b
f-velka <82233876+f-velka@users.noreply.github.com>
2022-07-11 01:49:22 +0900

    refactor printer

--- src/printer.ts
+++ src/printer.ts
@@ -3,85 +3,149 @@ import { Options } from './options';
 import { TextWriter } from './textWriter';
 import { calcValueWidth } from './utils';
 
-function formatCell(value: string, width: number, rowidth: number): string {
-	return 	`${value}${' '.repeat(rowidth - width)}`;
-}
-
-function formatCells(values: string[], maxWidths: number[]): string[] {
-	return values.map((v, i) => formatCell(v, calcValueWidth(v), maxWidths[i]));
-}
-
-function makeBlankCells(file: File, maxWidths: number[]): string[] {
-	const ret: string[] = [];
-	for (let i = file.columnCount; i < maxWidths.length; i++) {
-		ret.push(formatCell('', 0, maxWidths[i]));
-	}
-	return ret;
-}
-
 export interface PrettyPrinter {
 	print(file: File, maxWidths: number[], options: Options, writer: TextWriter): void;
 }
 
-class GridPrinter implements PrettyPrinter {
+/**
+ * Base class for pretty printer implementations.
+ *
+ * For example, in {@link GridPrinter}, abstract methods have responsibilities to
+ * print the parts as below.
+ *
+ * +----+----+----+----+ <= printHeader()
+ * | a  | b  | c  | d  | <= printHeaderRow()
+ * +----+----+----+----+ <= printHeaderRow()
+ * | 1  | 2  | 3  | 4  | <= printDataRow()
+ * | 11 | 12 | 13 | 14 | <= printDataRow()
+ * +----+----+----+----+ <= printFooter()
+ *
+ */
+abstract class PrinterBase implements PrettyPrinter {
 	print(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
-		const hGridLine = `+-${maxWidths.map(w => '-'.repeat(w)).join('-+-')}-+`;
-		const blankCells = makeBlankCells(file, maxWidths);
+		this.initialize(file, maxWidths, options, writer);
 
 		for (const preceding of file.precedings) {
 			writer.writeLine(preceding);
 		}
 
-		writer.writeLine(hGridLine);
+		this.printHeader(file, maxWidths, options, writer);
+		// writer.writeLine(hGridLine);
 		if (options.headerLocation === 'FirstRow' || options.headerLocation === 'Implicit') {
 			const headerRow = options.headerLocation === 'FirstRow' ?
 				file.firstRow : file.firstRow.map((_, i) => String(i));
-			const cells = formatCells(headerRow, maxWidths).concat(blankCells);
-			writer.writeLine(`| ${cells.join(' | ')} |`);
-			writer.writeLine(hGridLine);
+			this.printHeaderRow(file, maxWidths, options, writer, headerRow);
 		}
 
 		let rowIndex = options.headerLocation === 'FirstRow' ? 1 : 0;
 		for (; rowIndex < file.rowCount; rowIndex++) {
-			const row = file.records[rowIndex];
-			const cells = formatCells(row, maxWidths).concat(blankCells);
-			writer.writeLine(`| ${cells.join(' | ')} |`);
-			if (options.insertLineBetweenRows) {
-				writer.writeLine(hGridLine);
-			}
+			this.printDataRow(file, maxWidths, options, writer, rowIndex);
 		}
 
-		if (!options.insertLineBetweenRows) {
-			writer.writeLine(hGridLine);
-		}
+		this.printFooter(file, maxWidths, options, writer);
+	}
+
+	protected abstract initialize(file: File, maxWidths: number[], options: Options, writer: TextWriter): void;
+	protected abstract printHeader(file: File, maxWidths: number[], options: Options, writer: TextWriter): void;
+	protected abstract printHeaderRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, headerRow: string[]): void;
+	protected abstract printDataRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, rowIndex: number): void;
+	protected abstract printFooter(file: File, maxWidths: number[], options: Options, writer: TextWriter): void;
+
+	protected formatCell(value: string, width: number, rowidth: number): string {
+		return 	`${value}${' '.repeat(rowidth - width)}`;
+	}
+
+	protected formatCells(values: string[], maxWidths: number[]): string[] {
+		return values.map((v, i) => this.formatCell(v, calcValueWidth(v), maxWidths[i]));
 	}
 }
 
-class SimplePrinter implements PrettyPrinter {
-	print(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
-		for (const preceding of file.precedings) {
-			writer.writeLine(preceding);
-		}
+/**
+ * Pretty-printer implementation that prints records as below.
+ *
+ * +----+----+----+----+
+ * | a  | b  | c  | d  |
+ * +----+----+----+----+
+ * | 1  | 2  | 3  | 4  |
+ * | 11 | 12 | 13 | 14 |
+ * +----+----+----+----+
+ */
+class GridPrinter extends PrinterBase {
+	private _horizontalGridLine: string = '';
+	private _blankCells: string[] = [];
 
-		if (options.headerLocation === 'FirstRow' || options.headerLocation === 'Implicit') {
-			const headerRow = options.headerLocation === 'FirstRow' ?
-				file.firstRow : file.firstRow.map((_, i) => String(i));
-			const cells = formatCells(headerRow, maxWidths);
-			writer.writeLine(` ${cells.join('   ')}`);
-			// blank cells are literally blank in this mode.
-			// so we can cut them off.
-			writer.writeLine(maxWidths.slice(0, file.columnCount).map(w => '-'.repeat(w + 2)).join(' '));
-		}
+	protected initialize(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
+		this._horizontalGridLine = `+-${maxWidths.map(w => '-'.repeat(w)).join('-+-')}-+`;
+		this._blankCells = this.makeBlankCells(file, maxWidths);
+	}
 
-		let rowIndex = options.headerLocation === 'FirstRow' ? 1 : 0;
-		for (; rowIndex < file.rowCount; rowIndex++) {
-			const row = file.records[rowIndex];
-			const cells = formatCells(row, maxWidths);
-			writer.writeLine(` ${cells.join('   ').trimEnd()}`);
-			if (options.insertLineBetweenRows) {
-				writer.writeLine();
-			}
+	protected printHeader(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
+		writer.writeLine(this._horizontalGridLine);
+	}
+
+	protected printHeaderRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, headerRow: string[]): void {
+		const cells = this.formatCells(headerRow, maxWidths).concat(this._blankCells);
+		writer.writeLine(`| ${cells.join(' | ')} |`);
+		writer.writeLine(this._horizontalGridLine);
+	}
+
+	protected printDataRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, rowIndex: number): void {
+		const row = file.records[rowIndex];
+		const cells = this.formatCells(row, maxWidths).concat(this._blankCells);
+		writer.writeLine(`| ${cells.join(' | ')} |`);
+		if (options.insertLineBetweenRows) {
+			writer.writeLine(this._horizontalGridLine);
 		}
+	}
+
+	protected printFooter(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
+		if (!options.insertLineBetweenRows) {
+			writer.writeLine(this._horizontalGridLine);
+		}
+	}
+
+	private makeBlankCells(file: File, maxWidths: number[]): string[] {
+		const ret: string[] = [];
+		for (let i = file.columnCount; i < maxWidths.length; i++) {
+			ret.push(this.formatCell('', 0, maxWidths[i]));
+		}
+		return ret;
+	}
+}
+
+/**
+ * Pretty-printer implementation that prints records as below.
+ *
+ *  a    b    c    d
+ * ---- ---- ---- ----
+ *  1    2    3    4
+ *  11   12   13   14
+ */
+class SimplePrinter extends PrinterBase {
+	protected initialize(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
+	}
+
+	protected printHeader(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
+	}
+
+	protected printHeaderRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, headerRow: string[]): void {
+		const cells = this.formatCells(headerRow, maxWidths);
+		writer.writeLine(` ${cells.join('   ').trimEnd()}`);
+		// blank cells are literally blank in this mode.
+		// so we can cut them off.
+		writer.writeLine(maxWidths.slice(0, file.columnCount).map(w => '-'.repeat(w + 2)).join(' '));
+	}
+
+	protected printDataRow(file: File, maxWidths: number[], options: Options, writer: TextWriter, rowIndex: number): void {
+		const row = file.records[rowIndex];
+		const cells = this.formatCells(row, maxWidths);
+		writer.writeLine(` ${cells.join('   ').trimEnd()}`);
+		if (options.insertLineBetweenRows) {
+			writer.writeLine();
+		}
+	}
+
+	protected printFooter(file: File, maxWidths: number[], options: Options, writer: TextWriter): void {
 	}
 }
 
